0cfda640b48e4823e1493f9e7d993451dbc38efd
greg7mdp <greg7mdp@gmail.com>
2016-08-24 09:27:32 -0400

    Add LICENSE for repository, and simplify license in sparsepp.h

--- sparsepp.h
+++ sparsepp.h
@@ -3,48 +3,14 @@
 
 
 // ----------------------------------------------------------------------
-// Copyright (c) 2016, Steven Gregory Popovitch - greg7mdp@gmail.com
+// Copyright (c) 2016, Gregory Popovitch - greg7mdp@gmail.com
 // All rights reserved.
 // 
 // This work is derived from Google's sparsehash library
-// (see https://github.com/sparsehash/sparsehash) whose copyright appears
-// below this one.
-// 
-// Compiler detection code (SPP_ proprocessor macros) derived from Boost 
-// libraries. Boost software licence reproduced below.
 //
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are
-// met:
-//
-//     * Redistributions of source code must retain the above copyright
-// notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-// copyright notice, this list of conditions and the following disclaimer
-// in the documentation and/or other materials provided with the
-// distribution.
-//     * The name of Steven Gregory Popovitch may not be used to 
-// endorse or promote products derived from this software without 
-// specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-// OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-// LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-// DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// ----------------------------------------------------------------------
-
- 
-// ----------------------------------------------------------------------
 // Copyright (c) 2005, Google Inc.
 // All rights reserved.
-//
+// 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -72,6 +38,10 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 // ----------------------------------------------------------------------
 
+ 
+// ---------------------------------------------------------------------------
+// Compiler detection code (SPP_ proprocessor macros) derived from Boost 
+// libraries. Therefore Boost software licence reproduced below.
 // ---------------------------------------------------------------------------
 // Boost Software License - Version 1.0 - August 17th, 2003
 // 
