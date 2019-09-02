8798718b2b85dbde22f101888c68a696fc60e892
greg <greg7mdp@gmail.com>
2019-09-02 08:41:01 -0400

    Fix issue #82.

--- sparsepp/spp_utils.h
+++ sparsepp/spp_utils.h
@@ -409,7 +409,10 @@ public:
 
     pointer allocate(size_t n, const_pointer  /* unused */= 0) 
     {
-        return static_cast<pointer>(malloc(n * sizeof(T)));
+        pointer res = static_cast<pointer>(malloc(n * sizeof(T)));
+        if (!res)
+            throw std::bad_alloc();
+        return res;
     }
 
     void deallocate(pointer p, size_t /* unused */) 
@@ -419,7 +422,10 @@ public:
 
     pointer reallocate(pointer p, size_t new_size) 
     {
-        return static_cast<pointer>(realloc(p, new_size * sizeof(T)));
+        pointer res = static_cast<pointer>(realloc(p, new_size * sizeof(T)));
+        if (!res)
+            throw std::bad_alloc();
+        return res;
     }
 
     // extra API to match spp_allocator interface
