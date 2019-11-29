b0f244f6519b85406fdc01735f55a9dbc95c0f40
greg <greg7mdp@gmail.com>
2019-11-29 11:44:21 -0500

    Issue #84 - add conan recipe

---
+++ conanfile.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from conans import ConanFile, tools
+import os
+
+class SparseppConan(ConanFile):
+    name = "sparsepp"
+    version = "1.22"
+    description = "A fast, memory efficient hash map for C++"
+    
+    # Indicates License type of the packaged library
+    license = "https://github.com/greg7mdp/sparsepp/blob/master/LICENSE"
+    
+    # Packages the license for the conanfile.py
+    exports = ["LICENSE.md"]
+    
+    # Custom attributes for Bincrafters recipe conventions
+    source_subfolder = "source_subfolder"
+    
+    def source(self):
+        source_url = "https://github.com/greg7mdp/sparsepp"
+        tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
+        extracted_dir = self.name + "-" + self.version
+
+        #Rename to "source_folder" is a convention to simplify later steps
+        os.rename(extracted_dir, self.source_subfolder)
+
+
+    def package(self):
+        include_folder = os.path.join(self.source_subfolder, "sparsepp")
+        self.copy(pattern="LICENSE")
+        self.copy(pattern="*", dst="include/sparsepp", src=include_folder)
+
+    def package_id(self):
+        self.info.header_only()
