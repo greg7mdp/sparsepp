af579a90ea22b7666e5ed89c30755c8a9b1359fb
greg <greg7mdp@gmail.com>
2020-01-14 20:54:48 -0500

    correct fix for issue #86

--- sparsepp/spp.h
+++ sparsepp/spp.h
@@ -2084,10 +2084,11 @@ public:
             // resize group array
             // ------------------
             group_type *first = 0, *last = 0;
-            if (sz && old_sz)
+            if (sz)
             {
                 _alloc_group_array(sz, first, last);
-                memcpy(static_cast<void *>(first), _first_group, sizeof(*first) * (std::min)(sz, old_sz));
+                if (old_sz)
+                    memcpy(static_cast<void *>(first), _first_group, sizeof(*first) * (std::min)(sz, old_sz));
             }
 
             if (sz < old_sz)
