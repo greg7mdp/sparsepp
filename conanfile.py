bffc6c2b86b20e811229f5c5e9e33cd96211fe2b
Federico Fuga <fuga@studiofuga.com>
2025-05-14 18:09:40 +0200

    Fix conanfile package, add package test

--- conanfile.py
+++ conanfile.py
@@ -9,31 +9,20 @@ class SparseppConan(ConanFile):
     name = "sparsepp"
     version = "1.22"
     description = "A fast, memory efficient hash map for C++"
-    
-    # Indicates License type of the packaged library
     license = "https://github.com/greg7mdp/sparsepp/blob/master/LICENSE"
-    
-    # Packages the license for the conanfile.py
+
+    exports_sources = "sparsepp/*"
+    no_copy_source = True
     exports = ["LICENSE"]
-    
-    # Custom attributes for Bincrafters recipe conventions
-    source_subfolder = "source_subfolder"
-    
     package_type = "header-library"
-    
-    def source(self):
-        source_url = "https://github.com/greg7mdp/sparsepp"
-        get(self, "{0}/archive/{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = self.name + "-" + self.version
-
-        #Rename to "source_folder" is a convention to simplify later steps
-        os.rename(extracted_dir, self.source_subfolder)
-
 
     def package(self):
-        include_folder = os.path.join(self.source_subfolder, "sparsepp")
-        copy(self, "*", include_folder, self.package_folder)
+        copy(self, "*",
+             self.source_folder,
+             os.path.join(self.package_folder, "include"))
 
     def package_id(self):
         self.info.clear()
-    
+
+    def package_info(self):
+        self.cpp_info.includedirs = [os.path.join(self.package_folder, "include")]
